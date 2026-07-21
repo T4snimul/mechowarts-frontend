c131876b5016ae4f5b1dfa417e828fe8746ee438
Tasnimul Hasan <2408020@student.ruet.ac.bd>
2026-07-21 20:45:12 +0600

    FIX: good so far

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,22 +1,12 @@
-import type { Metadata } from "next";
-
-import RootHeader from "@/components/shared/root-header";
-import DashboardWelcome from "@/components/shared/dashboard-welcome";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Mechowarts | Dashboard",
-  description: "Dashboard has everything a student needs.",
+  description: "Dashboard has everything a student needs",
 };
 
 export default function Dashboard() {
   return (
-    <div className="selection:bg-primary selection:text-background from-background to-muted/40 flex min-h-screen flex-col bg-linear-to-b">
-      <RootHeader />
-      <main className="flex flex-1 items-center justify-center px-6 py-12">
-        <div className="w-full max-w-5xl px-4 py-6 sm:px-6">
-          <DashboardWelcome />
-        </div>
-      </main>
-    </div>
+    <div className="selection:bg-primary selection:text-background from-background to-muted/40 flex min-h-screen flex-col bg-linear-to-b"></div>
   );
 }
